a741364e293019169fb9548636f4534ee64ff161
Cedric Gagnon <cgagnon@coveo.com>
2016-07-12 12:43:42 -0400

    try something to auto deploy the search-ui

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,24 @@
 var gulp = require('gulp');
+var debug = require('gulp-debug');
 
-gulp.task('copySearchUi', function () {
-    return gulp.src(['css/**/*', 'image/**/*', 'js/**/*', '**/*.html'], { "base" : "./node_modules/coveo-search-ui/bin" })
-        .pipe(gulp.dest('./public/'))
+
+gulp.task('copyCustomUi', function () {
+    return gulp.src(['searchTest.html'], { "base" : "." })
+        .pipe(gulp.dest('./public'))
 });
+
+gulp.task("copySearchUi", function () {
+  return gulp.src([
+                    './node_modules/coveo-search-ui/bin/css/**/*',
+                    './node_modules/coveo-search-ui/bin/image/**/*',
+                    './node_modules/coveo-search-ui/bin/js/**/*',
+                    './node_modules/coveo-search-ui/bin/*.html'],
+                  { "base" : "./node_modules/coveo-search-ui/bin" })
+                  .pipe(gulp.dest('./public'))
+                  .pipe(debug());
+
+})
+
+gulp.task('default', ['copyCustomUi', 'copySearchUi'], function () {
+
+})
