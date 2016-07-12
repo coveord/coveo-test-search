b7132da750dda7350169ae5e4b475d8c5055bebc
Cedric Gagnon <cgagnon@coveo.com>
2016-07-12 10:31:38 -0400

    better static file serving now with dir listing

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 "use strict";
 var express = require('express');
+var serveIndex = require('serve-index')
+var serveStatic = require('serve-static')
 var app = express();
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static('public'));
+app.use(
+  serveStatic('public',
+    {'index': ['_.html']}
+  ),
+  serveIndex('public',
+    {'icons': true}
+  )
+)
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
