8c2ab04778fa7e487df32b00a068e2e664b7758d
Cedric Gagnon <cgagnon@coveo.com>
2016-07-11 23:07:25 -0400

    Simplified the test env

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-/// <reference path="typings/tsd.d.ts" />
 "use strict";
 var express = require('express');
-var routers = require('./routers');
 var port = process.argv[2] || 3000;
 var app = express();
-app.use('/', routers.router);
+app.use(express.static('public'));
 app.listen(port);
