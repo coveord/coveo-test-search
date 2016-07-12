800c1106413e14a26707a4d50c0a45936d89c10a
Cedric Gagnon <cgagnon@coveo.com>
2016-07-11 23:12:08 -0400

    Port must be set from env var

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 "use strict";
 var express = require('express');
-var port = 80;
 var app = express();
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static('public'));
-app.listen(port);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
