17f34a523001d24330d021b9a9f88b047ae8bef7
Cedric Gagnon <cgagnon@coveo.com>
2016-07-15 10:30:51 -0400

    Access-Control-Allow-Origin Access-Control-Allow-Methods Access-Control-Allow-Headers

--- server.js
+++ server.js
@@ -4,6 +4,23 @@ var serveIndex = require('serve-index')
 var serveStatic = require('serve-static')
 var app = express();
 app.set('port', (process.env.PORT || 5000));
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+
+app.use(allowCrossDomain);
 app.use(
   serveStatic('public',
     {'index': ['_.html']}
